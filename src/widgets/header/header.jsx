07e93f5fcf96c958f60d92271467a09730be758a
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-03-01 17:51:12 +0700

    header added

--- src/widgets/header/header.jsx
+++ src/widgets/header/header.jsx
@@ -1,11 +1,22 @@
 import "./header.css";
 import lamp from "../../shared/images/lamp.svg";
+import avatar from "../../shared/images/avatar.png";
 
 function Header() {
     return(
         <header>
-            <img src={lamp} alt="lamp" />
-            <h3>Банк идей</h3>
+            <a href="#">
+                <div className="logo">
+                    <img src={lamp} alt="logo" />
+                    <h3>Банк идей</h3>
+                </div>
+            </a>
+            <nav>
+                <a href="#">Поиск</a>
+                <a href="#">Новости</a>
+                <a href="#">Создать идею</a>
+                <a href="#"><img src={avatar} alt="avatar" /></a>
+            </nav> 
         </header>
     )
 }
