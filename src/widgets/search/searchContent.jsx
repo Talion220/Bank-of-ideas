4b9b095fc0810c4b60e57aea4ab5f0b9123b24af
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-03-02 22:16:17 +0700

    filters added

--- src/widgets/search/searchContent.jsx
+++ src/widgets/search/searchContent.jsx
@@ -6,12 +6,28 @@ function SearchContent() {
     <div className="SearchContent">
       <form action="" method="get">
         <div className="search-container">
-          <input type="text" name="Search" placeholder="Искать идеи..." maxLength={90}/>
+          <input type="text" name="Search" placeholder="Искать идеи..." maxLength={200}/>
           <button type="submit" className="search-button">
             <img src={searchIcon} alt="Search" className="search-icon"/>
           </button>
         </div>
       </form>
+
+      <div className="filters">
+          <form action="" method="get">
+            <button>За все время</button>
+            <button>За год</button>
+            <button>За месяц</button>
+            <button>За неделю</button>
+            <button>За день</button>
+            <button>Больше всего голосов</button>
+            <button>Меньше всего голосов</button>
+            <button>Самые обсуждаемые</button>
+            <button>Одобрено</button>
+            <button>Предложено</button>
+            <button>Отклонено</button>
+          </form>
+      </div>
     </div>
   );
 }
