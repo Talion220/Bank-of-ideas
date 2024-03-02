4dc8bbb340d96f6cd96a3bcb288cd38486a99fb6
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-03-02 21:36:28 +0700

    search added

--- src/widgets/search/searchContent.jsx
+++ src/widgets/search/searchContent.jsx
@@ -1,9 +1,19 @@
-function SearchContent(){
-    return(
-        <div>
-            ss
+import searchIcon from "../../shared/images/search.svg";
+import './searchContent.scss';
+
+function SearchContent() {
+  return (
+    <div className="SearchContent">
+      <form action="" method="get">
+        <div className="search-container">
+          <input type="text" name="Search" placeholder="Искать идеи..." maxLength={90}/>
+          <button type="submit" className="search-button">
+            <img src={searchIcon} alt="Search" className="search-icon"/>
+          </button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
 export default SearchContent;
