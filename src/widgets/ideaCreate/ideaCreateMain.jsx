9f870056d3b5fa142023ec86d4243972d5123362
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-03-04 01:00:30 +0700

    idea create page done

--- src/widgets/ideaCreate/ideaCreateMain.jsx
+++ src/widgets/ideaCreate/ideaCreateMain.jsx
@@ -1,4 +1,5 @@
 import './ideaCreateMain.scss';
+import pin from "../../shared/images/pin.svg";
 
 function IdeaCreateMain(){
     return(
@@ -10,7 +11,12 @@ function IdeaCreateMain(){
                 <input type="text" id='ideaTitle' name='ideaTitle' placeholder='Введите название'/>
 
                 <label htmlFor="ideaCategory">Категория идеи</label>
-                <input type="listbox" id='ideaCategory' name='ideaCategory' placeholder='Выберите категорию' />
+                <select name="ideaCategory" id='ideaCategory' defaultValue='default'>
+                    <option value='default' disabled>Выберите категорию</option>
+                    <option value="ideaCategory1">Категория 1</option>
+                    <option value="ideaCategory2">Категория 2</option>
+                    <option value="ideaCategory3">Категория 3</option>
+                </select>
 
                 <label htmlFor="ideaProblem">Проблема</label>
                 <textarea name="ideaProblem" id="ideaProblem" cols="30" rows="10" placeholder='Опишите проблему'></textarea>
@@ -24,19 +30,25 @@ function IdeaCreateMain(){
                 <label htmlFor="ideaFilial">Филиал</label>   
                 <select name="ideaFilial" id='ideaFilial' defaultValue='default'>
                     <option value='default' disabled>Выберите филиал</option>
-                    <option value="value1">Красноярск</option>
-                    <option value="value2">Москва</option>
-                    <option value="value3">Петербург</option>
+                    <option value="ideaFilial1">Красноярск</option>
+                    <option value="ideaFilial2">Москва</option>
+                    <option value="ideaFilial3">Петербург</option>
                 </select>
 
                 <label htmlFor="ideaAuthor">Автор</label>
                 <input type="text" name='ideaAuthor' id='ideaAuthor' placeholder='Перечислите авторов' />
 
-                <label htmlFor="ideaImg">Выберите обложку</label>
-                <input type="image" name='ideaImg' id='ideaImg'/>
+                <label htmlFor="ideaImg" className='pin'>
+                    <img src={pin} alt="" />
+                    Выберите обложку
+                </label>
+                <input type="file" name='ideaImg' id='ideaImg' accept="image/png, image/jpeg"/>
 
-                <label htmlFor="ideaFiles">Прикрепить файл</label>
-                <input type="file" name='ideaFiles' id='ideaFiles'></input>
+                <label htmlFor="ideaFiles" className='pin'>
+                    <img src={pin} alt="" />
+                    Прикрепить файлы
+                </label>
+                <input type="file" name='ideaFiles' id='ideaFiles' accept=".pdf, .doc, .docx" multiple />
 
                 <button type='submit'>Создать</button>
             </form>
