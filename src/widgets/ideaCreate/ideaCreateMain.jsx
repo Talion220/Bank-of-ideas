68ce0a43a187fdf22c5fca2cc998bb12e0e69cf5
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-03-03 22:04:20 +0700

    idea create half done

--- src/widgets/ideaCreate/ideaCreateMain.jsx
+++ src/widgets/ideaCreate/ideaCreateMain.jsx
@@ -2,8 +2,44 @@ import './ideaCreateMain.scss';
 
 function IdeaCreateMain(){
     return(
-        <div>
+        <div className='ideaCreateMain'>
             <h1>Создать новую идею</h1>
+            <form action="" method='post'>
+
+                <label htmlFor="ideaTitle">Название идеи</label>
+                <input type="text" id='ideaTitle' name='ideaTitle' placeholder='Введите название'/>
+
+                <label htmlFor="ideaCategory">Категория идеи</label>
+                <input type="listbox" id='ideaCategory' name='ideaCategory' placeholder='Выберите категорию' />
+
+                <label htmlFor="ideaProblem">Проблема</label>
+                <textarea name="ideaProblem" id="ideaProblem" cols="30" rows="10" placeholder='Опишите проблему'></textarea>
+
+                <label htmlFor="ideaSolution">Решение</label>
+                <textarea name="ideaSolution" id="ideaSolution" cols="30" rows="10" placeholder='Опишите решение'></textarea>
+
+                <label htmlFor="ideaResult">Результат</label>
+                <textarea name="ideaResult" id="ideaResult" cols="30" rows="10" placeholder='Опишите ожидаемый результат'></textarea>
+
+                <label htmlFor="ideaFilial">Филиал</label>   
+                <select name="ideaFilial" id='ideaFilial' defaultValue='default'>
+                    <option value='default' disabled>Выберите филиал</option>
+                    <option value="value1">Красноярск</option>
+                    <option value="value2">Москва</option>
+                    <option value="value3">Петербург</option>
+                </select>
+
+                <label htmlFor="ideaAuthor">Автор</label>
+                <input type="text" name='ideaAuthor' id='ideaAuthor' placeholder='Перечислите авторов' />
+
+                <label htmlFor="ideaImg">Выберите обложку</label>
+                <input type="image" name='ideaImg' id='ideaImg'/>
+
+                <label htmlFor="ideaFiles">Прикрепить файл</label>
+                <input type="file" name='ideaFiles' id='ideaFiles'></input>
+
+                <button type='submit'>Создать</button>
+            </form>
         </div>
     )
 }
