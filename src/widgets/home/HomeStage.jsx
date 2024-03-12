2000031a894594fa5279bc3d7d6ea8425c53ff0b
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-03-12 22:58:13 +0700

    HomeStage added

--- src/widgets/home/HomeStage.jsx
+++ src/widgets/home/HomeStage.jsx
@@ -1,44 +1,41 @@
 import arrowStage from '../../shared/images/arrowStage.svg'
+import './homeStage.scss';
 
 function HomeStage() {
   return (
-    <div>
+    <div className='homeStage'>
       <h1>
         Как это работает?        
       </h1>
       <div className='stageContainer'>
         <div className='stageElement'>
           <img src={arrowStage} alt="" />
-          <p>Публикация</p>
-          <div>Вносите предложения по улучшению процесса компании или вашего подразделения и делитесь лучшими практиками.</div>
+          <p className='stageElementTitle'>Публикация</p>
+          <p>Вносите предложения по улучшению процесса компании или вашего подразделения и делитесь лучшими практиками.</p>
         </div>
-      </div>
-      <div className='stageContainer'>
+      
         <div className='stageElement'>
           <img src={arrowStage} alt="" />
-          <p>Рассмотрение</p>
-          <div>Все предложения проходят экспертизу. Экспертом формируется оценка и заключение о возможности тиражирования.</div>
+          <p className='stageElementTitle'>Рассмотрение</p>
+          <p>Все предложения проходят экспертизу. Экспертом формируется оценка и заключение о возможности тиражирования.</p>
         </div>
-      </div>
-      <div className='stageContainer'>
+      
         <div className='stageElement'>
           <img src={arrowStage} alt="" />
-          <p>Поощрение</p>
-          <div>За одобренные предложения и лучшие практики выплачивается поощрение.</div>
+          <p className='stageElementTitle'>Поощрение</p>
+          <p>За одобренные предложения и лучшие практики выплачивается поощрение.</p>
         </div>
-      </div>
-      <div className='stageContainer'>
+     
         <div className='stageElement'>
           <img src={arrowStage} alt="" />
-          <p>Реализация</p>
-          <div>Одобренные экспертами предложения внедряются в жизнь компании и совершенствуют ежедневные процессы. Лучшие практики тиражируются на все общество.</div>
+          <p className='stageElementTitle'>Реализация</p>
+          <p>Одобренные экспертами предложения внедряются в жизнь компании и совершенствуют ежедневные процессы. Лучшие практики тиражируются на все общество.</p>
         </div>
-      </div>
-      <div className='stageContainer'>
+      
         <div className='stageElement'>
           <img src={arrowStage} alt="" />
-          <p>Вознаграждение за внедрение</p>
-          <div>По факту внедрения подтверждается полученный экономический эффект и выплачивается вознаграждение участникам.</div>
+          <p className='stageElementTitle'>Вознаграждение за внедрение</p>
+          <p>По факту внедрения подтверждается полученный экономический эффект и выплачивается вознаграждение участникам.</p>
         </div>
       </div>
     </div>
