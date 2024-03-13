63be2d4d82e65b18da77acc215059d25fca770f3
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-03-13 19:37:20 +0700

    HomeTop Html fixed

--- src/widgets/home/HomeTop.jsx
+++ src/widgets/home/HomeTop.jsx
@@ -2,6 +2,8 @@ import HomeTopAuthors from "./homeTop/HomeTopAuthors";
 import HomeTopFilials from "./homeTop/HomeTopFilials";
 import HomeTopBusinessProcesses from "./homeTop/HomeTopBusinessProcesses";
 
+import './homeTop.scss';
+
 function HomeTop() {
   return (
     <div className="homeTop">
