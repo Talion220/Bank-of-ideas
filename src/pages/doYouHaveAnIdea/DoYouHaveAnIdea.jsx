3d8fa638e1db96e898a83176017d2e6aee5aa0ff
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-03-15 21:07:46 +0700

    DoYouHaveAnIdea demo added

--- src/pages/doYouHaveAnIdea/DoYouHaveAnIdea.jsx
+++ src/pages/doYouHaveAnIdea/DoYouHaveAnIdea.jsx
@@ -1,9 +1,59 @@
-
+// import doYouHaveAnIdea from "../../shared/images/doYouHaveAnIdea.png";
+import "./doYouHaveAnIdea.scss";
 
 function DoYouHaveAnIdea() {
   return (
-    <div>DoYouHaveAnIdea</div>
-  )
+    <div className="doYouHaveAnIdea">
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="#">Идея по совершенствованию работы общества</a>
+            <ul>
+              <li>
+                <a href="#">Вы уже применяете это в работе?</a>
+                <ul>
+                  <li>
+                    <a href="#">
+                      Можете описать результат и проблему, которую необходимо
+                      сделать?
+                    </a>
+                    <ul>
+                      <li>
+                        <a href="#">Html</a>
+                        <ul>
+                          <li>
+                            <a href="#">Css</a>
+                            <ul>
+                              <li>
+                                <a href="#">Jquery</a>
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#">Graph</a>
+                    <ul>
+                      <li>
+                        <a href="#">Image</a>
+                        <ul>
+                          <li>
+                            <a href="#">Design</a>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default DoYouHaveAnIdea;
