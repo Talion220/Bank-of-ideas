65b081a2c55db88f21bdb72cc91e272343903377
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-03-18 12:37:56 +0700

    Not found page mantine rewritten

--- src/pages/notFoundPage/NotFoundPage.jsx
+++ src/pages/notFoundPage/NotFoundPage.jsx
@@ -1,11 +1,40 @@
-import './NotFoundPage.scss';
+import {
+  Image,
+  Container,
+  Title,
+  Text,
+  Button,
+  SimpleGrid,
+} from "@mantine/core";
+import { NavLink } from "react-router-dom";
+import image from "./image.svg";
+import classes from "./NotFoundPage.module.css";
 
-function NotFoundPage(){
-    return(
-        <div className="notFoundPage">
-            <h1>Упс... Страница не найдена...</h1>
+export default function NotFoundPage() {
+  return (
+    <Container className={classes.root}>
+      <SimpleGrid spacing={{ base: 40, sm: 80 }} cols={{ base: 1, sm: 2 }}>
+        <Image src={image} className={classes.mobileImage} />
+        <div>
+          <Title className={classes.title}>Что-то пошло не так...</Title>
+          <Text c="dimmed" size="lg">
+            Страница, которую вы пытаетесь открыть, не существует. Возможно, вы
+            неправильно ввели адрес или страница была перемещена на другой URL.
+            Если вы считаете, что это ошибка, обратитесь в службу поддержки.
+          </Text>
+          <Button
+            size="md"
+            mt="xl"
+            radius={16}
+            className={classes.control}
+            component={NavLink}
+            to="/"
+          >
+            На главную
+          </Button>
         </div>
-    )
+        <Image src={image} className={classes.desktopImage} />
+      </SimpleGrid>
+    </Container>
+  );
 }
-
-export default NotFoundPage;
