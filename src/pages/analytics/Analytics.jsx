a0c6ca0ddf1be075577a5f1ed5fb6990a860d2b8
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-04-03 01:50:57 +0700

    analytics main page added

--- src/pages/analytics/Analytics.jsx
+++ src/pages/analytics/Analytics.jsx
@@ -1,8 +1,57 @@
 import classes from "./analytics.module.css";
-import { Container } from "@mantine/core";
+import { Button, Container, Text, Title } from "@mantine/core";
+
+const data = [
+  {
+    id: 1,
+    title: "Всего идей подано",
+    stats: "456,133",
+    description:
+      "Общее количество идей, поданных в нашем приложении. Включает в себя идеи по улучшению процессов, лучшие практики, и рационализаторские предложения от наших сотрудников.",
+  },
+  {
+    id: 2,
+    title: "Всего идей внедрено",
+    stats: "2,175",
+    description:
+      "Количество идей, которые были успешно внедрены после их подачи. Эти идеи привели к конкретным изменениям или улучшениям в нашей компании.",
+  },
+  {
+    id: 3,
+    title: "Идей на одного сотрудника",
+    stats: "1,994",
+    description:
+      "Среднее количество идей, предложенных каждым сотрудником. Это показывает активность и творческий потенциал наших сотрудников в процессе поиска новых решений и улучшений.",
+  },
+  {
+    id: 4,
+    title: "Подается идей в день",
+    stats: "1,994",
+    description:
+      "Среднее количество идей, которые подаются в нашем приложении ежедневно. Это свидетельствует о постоянном потоке новых идей и о нашем стремлении к постоянному улучшению и инновациям.",
+  },
+];
 
 function Analytics() {
-  return <Container size="lg"></Container>;
+  const stats = data.map((stat) => (
+    <div key={stat.id} className={classes.stat}>
+      <Text className={classes.count}>{stat.stats}</Text>
+      <Text className={classes.title}>{stat.title}</Text>
+      <Text className={classes.description}>{stat.description}</Text>
+      <Button mt={"var(--mantine-spacing-md)"} c="blue" bg="white" radius={12}>
+        Подробнее...
+      </Button>
+    </div>
+  ));
+
+  return (
+    <Container className={classes.analytics} size="xl" mt={40}>
+      <Title className={classes.heading} c="white">
+        Аналитика и статистика: ваши идеи в действии
+      </Title>
+      <div className={classes.root}>{stats}</div>
+    </Container>
+  );
 }
 
 export default Analytics;
