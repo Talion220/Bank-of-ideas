51b2750c5bc33de40656900e42cf6df3a75af2d7
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-04-07 22:21:36 +0700

    footer upd

--- src/widgets/common/footer/Footer.jsx
+++ src/widgets/common/footer/Footer.jsx
@@ -1,72 +1,20 @@
-import { Text, Container, ActionIcon, Group, rem } from "@mantine/core";
+import { Text, Container, ActionIcon, Group, rem, Anchor } from "@mantine/core";
 import { Icons } from "../../../shared/images/Icons";
 import classes from "./footer.module.css";
 
-const data = [
-  {
-    title: "About",
-    links: [
-      { label: "Features", link: "#" },
-      { label: "Pricing", link: "#" },
-      { label: "Support", link: "#" },
-      { label: "Forums", link: "#" },
-    ],
-  },
-  {
-    title: "Project",
-    links: [
-      { label: "Contribute", link: "#" },
-      { label: "Media assets", link: "#" },
-      { label: "Changelog", link: "#" },
-      { label: "Releases", link: "#" },
-    ],
-  },
-  {
-    title: "Community",
-    links: [
-      { label: "Join Discord", link: "#" },
-      { label: "Follow on Twitter", link: "#" },
-      { label: "Email newsletter", link: "#" },
-      { label: "GitHub discussions", link: "#" },
-    ],
-  },
-];
-
 export default function FooterLinks() {
-  const groups = data.map((group) => {
-    const links = group.links.map((link, index) => (
-      <Text
-        key={index}
-        className={classes.link}
-        component="a"
-        href={link.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </Text>
-    ));
-
-    return (
-      <div className={classes.wrapper} key={group.title}>
-        <Text className={classes.title}>{group.title}</Text>
-        {links}
-      </div>
-    );
-  });
-
   return (
     <footer className={classes.footer}>
       <Container size="xl" px="md" className={classes.inner}>
-        <div className={classes.logo}>
-          <Icons.Logo />
-          <Text size="xs" className={classes.description}>
-            Build fully functional accessible web applications faster than ever
-          </Text>
-        </div>
-        <div className={classes.groups}>{groups}</div>
-      </Container>
-      <Container size="xl" px="md" className={classes.afterFooter}>
-        <Text size="sm">© 2024 dev. All rights reserved.</Text>
+        <Text size="sm">© Банк идей 2024. Все права защищены.</Text>
+        <Anchor
+          className={classes.link}
+          href="https://google.com"
+          target="_blank"
+          size="sm"
+        >
+          Техническая поддержка.
+        </Anchor>
       </Container>
     </footer>
   );
