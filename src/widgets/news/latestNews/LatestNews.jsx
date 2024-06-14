452b0dcb0d71517b13ba42103f5d3e3d4fb27f06
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-06-15 00:18:20 +0700

    latest news upd

--- src/widgets/news/latestNews/LatestNews.jsx
+++ src/widgets/news/latestNews/LatestNews.jsx
@@ -21,7 +21,19 @@ function LatestNews({ article }) {
   const date = formatDate(article.date);
   return (
     <Card withBorder radius="16" p={0} mb={20} className={classes.card}>
-      <Group wrap="nowrap" gap={0}>
+      <Anchor
+        component={NavLink}
+        to={`/news/${article.id}`}
+        onClick={() => {
+          scrollToTop();
+        }}
+        c="dark"
+        underline="none"
+        className={classes.link}
+      >
+        <Image radius="16" src={article.newsImg} className={classes.img} />
+      </Anchor>
+      <Flex direction="column" className={classes.body}>
         <Anchor
           component={NavLink}
           to={`/news/${article.id}`}
@@ -29,90 +41,76 @@ function LatestNews({ article }) {
             scrollToTop();
           }}
           c="dark"
-          underline="none"
         >
-          <Image radius="16" src={article.newsImg} h={300} w={300} />
-        </Anchor>
-        <Flex direction="column" className={classes.body}>
-          <Anchor
-            component={NavLink}
-            to={`/news/${article.id}`}
-            onClick={() => {
-              scrollToTop();
+          <Tooltip
+            color="gray.3"
+            multiline
+            position="top-start"
+            // withArrow
+            transitionProps={{
+              transition: "pop-bottom-left",
+              duration: 200,
             }}
-            c="dark"
+            c="black"
+            label={article.title}
           >
-            <Tooltip
-              color="gray.3"
-              multiline
-              position="top-start"
-              // withArrow
-              transitionProps={{
-                transition: "pop-bottom-left",
-                duration: 200,
-              }}
-              c="black"
-              label={article.title}
-            >
-              <Text className={classes.title} lineClamp={2} mt="xs">
-                {article.title}
-              </Text>
-            </Tooltip>
-          </Anchor>
-          <Group pt={15} wrap="nowrap" gap="xs">
-            <Flex gap="sm" wrap="wrap" align="center" mt={5}>
-              <Anchor
-                component={NavLink}
-                to="/profile"
-                onClick={() => {
-                  scrollToTop();
-                }}
-                c="dark"
-                className={classes.link}
-              >
-                <Group gap="xs" wrap="nowrap">
-                  <Avatar size={20} src={article.avatar} />
-                  <Text size="xs">{article.author}</Text>
-                </Group>
-              </Anchor>
-              <Text size="xs" c="dimmed">
-                •
-              </Text>
-              <Text size="xs" c="dimmed">
-                {date}
-              </Text>
-              <Text size="xs" c="dimmed">
-                •
-              </Text>
-              <Flex align="center" gap={5} size="xs" c="dimmed">
-                <Icons.IconEye />
-                <Text size="xs">{article.views}</Text>
-              </Flex>
-            </Flex>
-          </Group>
-          <Group pt={15} gap="lg">
-            <Like id={article.id} from={"news"} />
-            <Button
-              variant="light"
-              c="gray"
-              radius="16"
-              color="blue"
+            <Text className={classes.title} lineClamp={2} mt="xs">
+              {article.title}
+            </Text>
+          </Tooltip>
+        </Anchor>
+        <Group pt={15} wrap="nowrap" gap="xs">
+          <Flex gap="sm" wrap="wrap" align="center" mt={5}>
+            <Anchor
               component={NavLink}
-              to={`/news/${article.id}?fromLink=true`}
+              to="/profile"
               onClick={() => {
                 scrollToTop();
               }}
+              c="dark"
             >
-              <Center>
-                <Icons.IconMessageCircle />
-                <Text size="sm" className={classes.bodyText}>
-                  {article.comments.length}
-                </Text>
-              </Center>
-            </Button>
-          </Group>
-        </Flex>
-      </Group>
+              <Group gap="xs" wrap="nowrap">
+                <Avatar size={20} src={article.avatar} />
+                <Text size="xs">{article.author}</Text>
+              </Group>
+            </Anchor>
+            <Text size="xs" c="dimmed">
+              •
+            </Text>
+            <Text size="xs" c="dimmed">
+              {date}
+            </Text>
+            <Text size="xs" c="dimmed">
+              •
+            </Text>
+            <Flex align="center" gap={5} size="xs" c="dimmed">
+              <Icons.IconEye />
+              <Text size="xs">{article.views}</Text>
+            </Flex>
+          </Flex>
+        </Group>
+        <Group pt={15} gap="lg">
+          <Like id={article.id} from={"news"} />
+          <Button
+            variant="light"
+            c="gray"
+            radius="16"
+            color="blue"
+            component={NavLink}
+            to={`/news/${article.id}?fromLink=true`}
+            onClick={() => {
+              scrollToTop();
+            }}
+          >
+            <Center>
+              <Icons.IconMessageCircle />
+              <Text size="sm" className={classes.bodyText}>
+                {article.comments.length}
+              </Text>
+            </Center>
+          </Button>
+        </Group>
+      </Flex>
     </Card>
   );
 }
